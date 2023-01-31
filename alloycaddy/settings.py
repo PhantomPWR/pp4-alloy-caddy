4ab81142a4c5b4a0c3b211a8cae7aa7db6db24a9
Jean de Villiers <jeandevilliers@me.com>
2023-01-31 19:18:53 +0000

    Fix css not loading on heroku

--- alloycaddy/settings.py
+++ alloycaddy/settings.py
@@ -103,6 +103,18 @@ DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
 
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
