a16a337d1bee1afc6aab507acd13376c6b1d85dc
Jean de Villiers <jeandevilliers@me.com>
2023-01-31 14:32:00 +0000

    Set DEBUG=False

--- alloycaddy/settings.py
+++ alloycaddy/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'https://8000-phantompwr-pp4alloycadd-7vwtxsnizp7.ws-eu82.gitpod.io',
