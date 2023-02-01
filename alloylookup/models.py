71973af757bb11fedeb3b59b9a8fda0a6a31e868
Jean de Villiers <jeandevilliers@me.com>
2023-02-01 17:30:16 +0000

    Try ArrayField

--- alloylookup/models.py
+++ alloylookup/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-# from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -71,7 +71,6 @@ class Subcategory(models.Model):
     class Meta:
         verbose_name_plural = "Subcategories"
 
-
     subcategory_id = models.IntegerField(
         null=False,
         blank=False
@@ -92,7 +91,7 @@ class Category(models.Model):
     """
 
     class Meta:
-            verbose_name_plural = "Categories"
+        verbose_name_plural = "Categories"
 
     category_id = models.IntegerField(
         null=False,
@@ -159,21 +158,21 @@ class Alloy(models.Model):
         blank=True
     )
 
-    # ELEMENT_CHOICES = (
-    #     ('si_min', 'Si Min'),
-    #     ('si_max', 'Si Max')
-    # )
+    ELEMENT_CHOICES = (
+        ('si_min', 'Si Min'),
+        ('si_max', 'Si Max')
+    )
 
-    # elements = ArrayField(
-    #     models.CharField(
-    #         choices=ELEMENT_CHOICES,
-    #         max_length=10,
-    #         blank=True,
-    #         null=True
-    #     ),
-    #     blank=True,
-    #     null=True
-    # )
+    elements = ArrayField(
+        models.CharField(
+            choices=ELEMENT_CHOICES,
+            max_length=10,
+            blank=True,
+            null=True
+        ),
+        blank=True,
+        null=True
+    )
 
 
     # Elements
