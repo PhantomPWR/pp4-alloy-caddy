f0238de803f29c57f5b595ac333c43742b9e05ed
Jean de Villiers <jeandevilliers@me.com>
2023-03-06 17:22:30 +0000

    Python validation check

---
+++ alloylookup/forms.py
@@ -0,0 +1,64 @@
+"""
+ModelForms for AlloyLookup App
+"""
+from django.contrib.auth import get_user_model
+from django.forms import ModelForm
+from django import forms
+from models import (
+    Alloy,
+    Category,
+    Subcategory,
+    PrimaryFootnote,
+    SecondaryFootnote,
+    Country
+    )
+
+
+class CreateAlloyForm(ModelForm):
+    """
+    ModelForm for adding alloys
+    """
+    class Meta:
+        """
+        Meta class for CreateAlloyForm
+        """
+        model = Alloy
+        fields = [
+            'alloy_code',
+            'category',
+            'subcategory',
+            'alloy_description',
+            'primary_footnote_id',
+            'secondary_footnote_id',
+            'country',
+            'alloy_elements'
+            ]
+
+        widgets = {
+            'alloy_code': forms.NumberInput(
+                attrs={'class': 'form-control'}
+                ),
+            'category': forms.Select(
+                attrs={'class': 'form-control'}
+                ),
+            'subcategory': forms.Select(
+                attrs={'class': 'form-control'}
+                ),
+            'alloy_description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'cols': '80', 'rows': '20'
+                    }
+                ),
+            'primary_footnote_id': forms.Select(
+                attrs={'class': 'form-control'}
+                ),
+            'secondary_footnote_id': forms.Select(
+                attrs={'class': 'form-control'}
+                ),
+            'country': forms.Select(attrs={'class': 'form-control'}),
+            'alloy_elements': forms.JSONField(
+                attrs={'class': 'form-control'},
+                default=dict
+                ),
+        }
