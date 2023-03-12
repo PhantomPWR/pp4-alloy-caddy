b6552a2cfade4a19e45c8a747375274a52780a7e
Jean de Villiers <jeandevilliers@me.com>
2023-03-12 16:32:22 +0000

    Show/hide menu items based on user permissions

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import (
+    authenticate,
+    login,
+    logout,
+    get_user_model
+    )
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404
+    )
 
 # Create your views here.
 
